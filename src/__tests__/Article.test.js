8bff7224316b2c7e15682f1ae9d5d8d43f910752
Red <red.sarouf2014@gmail.com>
2023-05-20 23:41:13 -0400

    Uncomment div in Arcticle.js

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -4,8 +4,18 @@ import React from "react";
 
 import Article from "../components/Article";
 
+// test("displays the text 'please pass this test'", () => {
+//   render(<Article />);
+
+//   expect(screen.queryByText("please pass this test")).toBeInTheDocument();
+// });
+
 test("displays the text 'please pass this test'", () => {
   render(<Article />);
 
-  expect(screen.queryByText("please pass this test")).toBeInTheDocument();
+  const element = screen.queryByText("please pass this test");
+
+  screen.debug(element);
+
+  expect(element).toBeInTheDocument();
 });
